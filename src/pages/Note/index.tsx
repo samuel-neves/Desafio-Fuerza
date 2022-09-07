1c49ac1367ca97d1d9daa6d4bb7e5e39a6e8cee6
Samuel Oliveira das Neves <sneves@gyramais.com>
2022-09-07 15:11:11 -0300

    feat: note details page implementation

--- src/pages/Note/index.tsx
+++ src/pages/Note/index.tsx
@@ -1,10 +1,87 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { TextField } from '@mui/material';
+import { useHistory, useParams } from 'react-router-dom';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
-import {Container} from './styles';
+import {
+  Container,
+  LogoContainer,
+  HeaderContainer,
+  NoteContainer,
+} from './styles';
+
+import { Logo } from '../../assets'
+import { useAuth } from '../../hooks/useAuth';
+import { Journal } from '../../interfaces/journal.interface'
+import { Entry } from '../../interfaces/entry.interface'
+import http from '../../services/api';
+
+interface JournalParams {
+  journalId: string;
+  noteId: string;
+}
 
 const Note: React.FC = () => {
+  const [journal, setJournal] = useState<Journal>()
+  const [note, setNote] = useState<Entry>()
+  const { user, logout } = useAuth();
+  const history = useHistory();
+  const { journalId, noteId } = useParams<JournalParams>()
+
+  useEffect(() => {
+    http.get(`/journals/${user.id}`).then((response: any) => {
+      if (!response) {
+        logout()
+        return null
+      }
+
+      setJournal(
+        response.journals.filter(
+          (journal: Journal) => journal.id === journalId
+        )[0] || null
+      )
+    })
+  }, [journalId, logout, user.id])
+
+  useEffect(() => {
+    http.get(`/journals/entries/${journalId}`).then((response: any) => {
+      if (!response) {
+        logout()
+        return null
+      }
+
+      setNote(
+        response.entries.filter(
+          (note: Entry) => note.id === noteId
+        )[0] || null
+      )
+    })
+  }, [journalId, noteId, logout])
+
   return(
-    <Container>Note</Container>
+    <Container>
+      <LogoContainer>
+        <img src={Logo} alt="Logo" />
+      </LogoContainer>
+      <HeaderContainer onClick={() => history.goBack()}>
+        <KeyboardArrowLeftIcon />
+        <h2>{journal?.title}</h2>
+      </HeaderContainer>
+      <NoteContainer>
+        <TextField
+          value={note?.title}
+          variant='filled'
+          multiline
+          disabled
+        />
+        <TextField
+          value={note?.content}
+          variant='filled'
+          multiline
+          disabled
+        />
+      </NoteContainer>
+    </Container>
   )
 }
 
--- src/pages/Note/styles.ts
+++ src/pages/Note/styles.ts
@@ -1,3 +1,71 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 0 26px;
+  height: fit-content;
+
+  margin: 0 auto;
+  max-width: 500px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 44px;
+`;
+
+export const HeaderContainer = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  margin-left: -4px;
+  margin-top: 32px;
+
+  h2 {
+    font-size: 24px;
+    font-family: 'Abhaya Libre';
+    font-weight: 700;
+    margin-left: 12px;
+  }
+
+  svg {
+    position: relative;
+    top: -1px;
+    font-size: 34px;
+    font-weight: 400;
+  }
+`;
+
+export const NoteContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  button {
+    margin: 0 auto;
+    margin-top: 42px;
+  }
+
+  .MuiInputBase-root.MuiFilledInput-root, .MuiInputBase-root {
+    margin-top: 28px;
+    background-color: #ffffff;
+    padding-top: 0;
+    padding-bottom: 0;
+    padding-right: 0;
+    padding-left: 0;
+
+    &::before {
+      border: none;
+    }
+
+    textarea {
+      color: #834825;
+      padding-top: 12px;
+      padding-right: 12px;
+      padding-bottom: 12px;
+      padding-left: 12px;
+      -webkit-text-fill-color: #834825;
+    }
+  }
+`;
